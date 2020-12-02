7ead8ad837f134f518ef4b8411d4ffb4046f1c73
Connor Brubaker <brubaker@tamu.edu>
2020-12-02 17:44:42 -0600

    Wrote/prelim testing on sliding window function

---
+++ R/sliding_window.R
@@ -0,0 +1,17 @@
+sliding_window <- function(X, window_size) {
+    # dimension parameters
+    dim_ts <- dim(X)[1]
+    N_points <- dim(X)[2]
+    N_subsequences <- N_points - window_size + 1
+
+    # create template matrix to fill in
+    W <- matrix(0, nrow = dim_ts * window_size, ncol = N_subsequences)
+
+    # fill in the result
+    for (i in 1:N_subsequences) {
+        W[ , i] <- as.vector(X[ , i:(i + window_size - 1)])
+    }
+
+    # return
+    return(W)
+}
