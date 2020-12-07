a26cddff6e5853127e5375446866bd1f0368f9dc
Connor Brubaker <brubaker@tamu.edu>
2020-12-06 23:29:30 -0600

    Added documentaiton to gaussian_kernel

--- R/gaussian_kernel.R
+++ R/gaussian_kernel.R
@@ -1,3 +1,10 @@
+#' Gaussian kernel function
+#'
+#' @param distance_matrix Matrix of squared euclidean matrices
+#' @param sigma Kernel bandwidth parameter
+#'
+#' @return Gaussian kernel applied to the input matrix
+#'
 gaussian_kernel <- function(distance_matrix, sigma) {
     exp(-distance_matrix / (2 * sigma ^ 2))
 }
